f4aca074dab77920f9763f58e2e74c08d98e3156
Rune Langseid <runelangseid@gmail.com>
2022-10-06 19:45:10 +0200

    Refactored a lot

--- futurehome2mqtt/pyfimptoha/client.py
+++ futurehome2mqtt/pyfimptoha/client.py
@@ -1,48 +1,27 @@
 import json, time
 import paho.mqtt.client as mqtt
 import requests, threading
-from pyfimptoha.light import Light
-from pyfimptoha.sensor import Sensor
-from pyfimptoha.switch import Switch
 from pyfimptoha.mode import Mode
 
 class Client:
-    components = {}
-    lights = []
-    sensors = []
-    switches = []
 
     _devices = [] # discovered fimp devices
-    _hassio_token = None
-    _ha_host = 'hassio'
     _mqtt = None
     _selected_devices = None
     _topic_discover = "pt:j1/mt:rsp/rt:app/rn:homeassistant/ad:flow1"
     _topic_fimp_event = "pt:j1/mt:evt/rt:dev/rn:zw/ad:1/"
     _topic_ha = "homeassistant/"
-    _uptime = 0
     _verbose = False
-    _listen_ha = False
-    _listen_fimp_event = False
 
     def __init__(self, mqtt=None, selected_devices=None, token=None, ha_host=None, debug=False):
         self._selected_devices = selected_devices
-        self._hassio_token = token
         self._verbose = debug
-
-        if ha_host:
-            self._ha_host = ha_host
 
         if mqtt:
             self._mqtt = mqtt
             mqtt.on_message = self.on_message
 
         self.start()
-
-        if self._hassio_token:
-            threading.Timer(20, self.check_restarts).start()
-            # self.check_restarts()
-
 
     def log(self, s, verbose = False):
         '''Log'''
@@ -54,7 +33,6 @@ class Client:
         # Add Modus sensor (home, sleep, away and vacation)
         # todo Find a way to auto discover the value. Sensor value is currently
         # empty until changed by the system/user
-        self.check_restarts()
 
         mode = Mode()
         message = mode.get_component()
@@ -62,66 +40,9 @@ class Client:
 
         # fimp discover
         self.send_fimp_discovery()
-        time.sleep(2)
-        self.publish_components()
-        time.sleep(2)
-        # Listen on fimp and homeassistant topics
-        self.listen_ha()
-        self.listen_fimp()
 
-    def check_restarts(self):
-        endpoint_prefix = ''
-        if self._ha_host == 'hassio':
-            endpoint_prefix = 'homeassistant/'
-
-        sensor_uptime = 'ha_uptime_minutes'
-        url = 'http://%s/%sapi/states/sensor.%s' % (self._ha_host, endpoint_prefix, sensor_uptime)
-        headers={
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + self._hassio_token,
-        }
-
-        uptime = None
-        try:
-            r = requests.get(
-                url,
-                headers=headers
-            )
-
-            if r.status_code == 200:
-                json = r.json()
-                uptime = int(float(json['state']))
-                print("Check for HA restart: Current uptime %s minutes" % str(uptime))
-            elif r.status_code == 401:
-                print("Check for HA restart: Home Assistant Rest API returned 401: Not authorize. Was a long-lived access token set up as mentioned in the README?")
-            elif r.status_code == 404:
-                print("Check for HA restart: Home Assistant Rest API returned 404: Not found. Sensor `%s` was not found. Was sensor `HA uptime moment` added as mentioned in the README?" % (sensor_uptime))
-
-        except requests.exceptions.RequestException as e:
-            print("Check for HA restart: Could not contact HA for uptime details")
-            print("Error code: " + r.status_code)
-            print(e)
-        except:
-            print('Something went wrong')
-
-
-        # if not self._uptime or self._uptime > uptime:
-        if uptime == None:
-            print("check_restarts: Could not get uptime from HA. Trying again in 60 seconds")
-            threading.Timer(60, self.check_restarts).start()
-            return
-
-        if self._uptime == 0:
-            self._uptime = uptime
-
-        if self._uptime > uptime:
-            print("check_restarts: HA has restarted. Doing auto discovery")
-            self._uptime = uptime
-            self.start()
-
-        # print('check_restarts: Uptime end', self._uptime)
-
-        threading.Timer(60, self.check_restarts).start()
+        # subscribe to home assistant status where ha announces restarts
+        self._mqtt.subscribe("homeassistant/status")
 
     # The callback for when a PUBLISH message is received from the server.
     def on_message(self, client, userdata, msg):
@@ -131,17 +52,9 @@ class Client:
         if msg.topic == self._topic_discover:
             data = json.loads(payload)
             self.create_components(data["val"]["param"]["device"])
-
-        # Received commands from HA
-        elif msg.topic.startswith(self._topic_ha):
-            self.process_ha(msg.topic, payload)
-
-        # Received event from FIMP - Update HA state_topic
-        # pt:j1/mt:evt/rt:dev/rn:zw/ad:1/
-        elif msg.topic.startswith(self._topic_fimp_event):
-            # print('Got fimp event !')
-            messages = self.process_fimp_event(msg.topic, payload)
-            self.publish_messages(messages)
+        elif msg.topic == "homeassistant/status" and payload == "online":
+            print(msg.topic)
+            self.start()
 
     def publish_messages(self, messages):
         """Publish list of messages over MQTT"""
@@ -169,41 +82,6 @@ class Client:
             self.log('Asking FIMP to expose all devices...')
             self.publish_messages([message])
 
-    def publish_components(self):
-        """
-        Publish components and their initial states
-        """
-
-        # - Lights
-        # todo Add support for Wall plugs with functionality 'Lighting'
-        for unique_id, component in self.components.items():
-            message = component.get_component()
-            self.publish_messages([message])
-
-        # Publish init states
-        print("Publishing init states")
-        print("- lights")
-        time.sleep(0.5)
-        for light in self.lights:
-            init_state = light.get_state()
-            for data in init_state:
-                self.publish_messages([data])
-                time.sleep(0.1)
-
-        print("- switches")
-        time.sleep(0.5)
-        for switch in self.switches:
-            init_state = switch.get_init_state()
-            self.publish_messages(init_state)
-            time.sleep(0.1)
-
-        print("- sensors")
-        time.sleep(0.5)
-        for sensor in self.sensors:
-            init_state = sensor.get_init_state()
-            self.publish_messages(init_state)
-            time.sleep(0.1)
-
     def load_json_device(self, filename):
         data = "{}"
 
@@ -213,10 +91,17 @@ class Client:
         self._devices.append(data)
 
     def create_components(self, devices):
-        """ Creates HA components out of FIMP devices"""
+        """
+        Creates HA components out of FIMP devices
+        by pushing them to HA using mqtt discovery
+        """
         self._devices = devices
         self.log('Received list of devices from FIMP. FIMP reported %s devices' % (len(devices)))
         self.log('Devices without rooms are ignored')
+
+        statuses = []
+
+        # todo Add support for Wall plugs with functionality 'Lighting'
 
         for device in self._devices:
             address = device["fimp"]["address"]
@@ -237,116 +122,121 @@ class Client:
             self.log("Creating: %s %s" % (address, name))
             self.log("- Functionality: %s" % (functionality))
 
+            #if address != '21':
+            #   continue
+
             for service_name in device["services"]:
-                component = None
-                component_address = address + "-" + service_name
-                service = device["services"][service_name]
+                # Service meter_elec - "Forbruk"
+                if service_name == "meter_elec":
+                    identifier = f"fh_{address}_meter_elec"
+                    state_topic = f"pt:j1/mt:evt/rt:dev/rn:zw/ad:1/sv:meter_elec/ad:{address}_0"
+                    component = {
+                        "name": f"{device['client']['name']} (forbruk)",
+                        "object_id": identifier,
+                        "unique_id": identifier,
+                        "state_topic": state_topic,
+                        "device_class": "energy",
+                        "state_class": "total_increasing",
+                        "unit_of_measurement": "kWh",
+                        "value_template": "{{ value_json.val }}"
+                    }
+                    payload = json.dumps(component)
+                    self._mqtt.publish(f"homeassistant/sensor/{identifier}/config", payload)
 
-                if (
-                    functionality == "lighting" and (
-                        service_name.startswith("out_bin_switch") or
-                        service_name.startswith("out_lvl_switch")
-                    )
-                ):
-                    component = "light"
-                elif functionality == "appliance" and (
-                    service_name.startswith("out_bin_switch")
-                ):
-                    component = "switch"
-                elif service_name in Sensor.supported_services():
-                    component = "sensor"
+                    # Queue statuses
+                    if device.get("param") and device['param'].get('energy'):
+                        value = device['param']['energy']
+                        data = {
+                            "props": {
+                                "unit": "kWh"
+                            },
+                            "serv": "meter_elec",
+                            "type": "evt.meter.report",
+                            "val": value,
+                        }
 
-                if not component:
-                    self.log("- Skipping %s. Not yet supported" % service_name, True)
-                    continue
+                        payload = json.dumps(data)
+                        statuses.append((state_topic, payload))
 
-                self.log("- Creating component %s - %s" % (component, service_name), True)
+                # Lights
+                elif functionality == "lighting":
+                    # todo Add support for bin switch
+                    if service_name == "out_lvl_switch":
+                        identifier = f"fh_{address}_{service_name}"
+                        command_topic = f"pt:j1/mt:cmd/rt:dev/rn:zw/ad:1/sv:out_lvl_switch/ad:{address}_1"
+                        state_topic = f"pt:j1/mt:evt/rt:dev/rn:zw/ad:1/sv:out_lvl_switch/ad:{address}_1"
+                        component = {
+                            "name": f"{device['client']['name']}",
+                            "object_id": identifier,
+                            "unique_id": identifier,
+                            "schema": "template",
+                            "command_topic": command_topic,
+                            "state_topic": state_topic,
+                            "brightness_scale": 100,
+                            "command_on_template": """
+                                {
+                                  "props":{},
+                                  "serv":"out_lvl_switch",
+                                  "tags":[]
+                                {%- if brightness -%}
+                                  , "type":"cmd.lvl.set",
+                                  "val":{{ (brightness / 2.55) | int }},
+                                  "val_t":"int"
+                                {%- else -%}
+                                  , "type":"cmd.binary.set",
+                                  "val":true,
+                                  "val_t":"bool"
+                                {%- endif -%}
+                                }
+                            """,
+                            "command_off_template": """
+                                {"props":{},
+                                  "serv":"out_lvl_switch",
+                                  "tags":[],
+                                  "type":"cmd.binary.set",
+                                  "val":false,
+                                  "val_t":"bool"}
+                            """,
+                            "state_template": "{% if value_json.val %} on {% else %} off {% endif %}",
+                            "brightness_template": "{% if value_json.val_t %}{{ (value_json.val * 2.55) | int }}{% endif %}"
+                        }
+                        payload = json.dumps(component)
+                        self._mqtt.publish(f"homeassistant/light/{identifier}/config", payload)
 
-                # todo Add support for binary_sensor
-                if component == "sensor":
-                    sensor = Sensor(service_name, service, device)
-                    self.sensors.append(sensor)
-                    self.components[sensor.unique_id] = sensor
-                elif component == "switch":
-                    switch = Switch(service_name, service, device)
-                    self.switches.append(switch)
-                    self.components[switch.unique_id] = switch
-                elif component == "light":
-                    light = Light(service_name, service, device)
-                    self.lights.append(light)
-                    self.components[light.unique_id] = light
+                        # Queue statuses
+                        if device.get("param") and device['param'].get('power'):
+                            power = device['param']['power']
+                            if power == "off":
+                                data = {
+                                    "props": {},
+                                    "serv": "out_lvl_switch",
+                                    "type": "cmd.binary.report",
+                                    "val_t": "bool",
+                                    "val": False
+                                }
+                            else:
+                                dim_value = device['param']['dimValue']
+                                data = {
+                                    "props": {},
+                                    "serv": "out_lvl_switch",
+                                    "type": "cmd.lvl.report",
+                                    "val_t": "int",
+                                    "val": dim_value
+                                }
+                            payload = json.dumps(data)
+                            statuses.append((state_topic, payload))
 
-    def process_ha(self, topic, payload):
-        """
-        Process a message from ha and generates one to FIMP
+                # todo Add support for binary_sensor?
 
-        Eg: 
-        - Topic: homeassistant/light/7-out_lvl_switch/set
-        - Payload: ON
-
-        Or set dimmer level
-        - Topic: homeassistant/light/7-out_lvl_switch/command
-        - Payload: 33
-        """
-
-        topic = topic.split("/")
-        result = None
-
-        # Topic like: homeassistant/light/7-out_lvl_switch/command
-        if len(topic) > 3:
-            # print("process_ha:topic", topic)
-            component = topic[1]
-            unique_id = topic[2]
-            topic_type = topic[3]
-
-            if component == "light":
-                for light in self.lights:
-                    if light.unique_id == unique_id:
-                        messages = light.handle_ha(topic_type, payload)
-
-                        if self._verbose:
-                            print("process ha: messages", messages)
-                        self.publish_messages(messages)
-
-        return
-
-    def process_fimp_event(self, topic, payload):
-        """
-        Process a message from FIMP and generates one to HA
-
-        Eg: Dimmer update
-        - Topic: pt:j1/mt:evt/rt:dev/rn:zw/ad:1/sv:out_lvl_switch/ad:7_1
-
-        """
-        topic = topic.split("/")
-        payload = json.loads(payload)
-
-        if len(topic) > 6:
-            tmp, type = topic[1].split(":", 1) # evt or cmd
-            tmp, service_name = topic[5].split(":", 1) # out_lvl_switch, sensor_power, etc
-            tmp, address = topic[6].split(":", 1)
-            address, tmp = address.split("_", 1) # device address: 1 or higher
-            unique_id = address + "-" + service_name
-
-            # debug Exclude all but light 7
-            # if address != "7":
-            #     return None
-
-            component = self.components.get(unique_id)
-            if not component:
-                return None
-
-            data = component.handle_fimp(payload)
-            return data
-
-        return None
-
-    def listen_ha(self):
-        """
-        Enables listening on HA topics
-        """
-        self._listen_ha = True
-        self._mqtt.subscribe(self._topic_ha + "#")
+        self._mqtt.loop()
+        time.sleep(2)
+        self.log("Publishing statuses...")
+        for state in statuses:
+            topic = state[0]
+            payload = state[1]
+            self._mqtt.publish(topic, payload)
+            self.log(topic)
+        self.log("Finished pushing statuses...")
 
     def listen_fimp(self):
         """
--- futurehome2mqtt/run.py
+++ futurehome2mqtt/run.py
@@ -5,6 +5,8 @@ import pyfimptoha.client as fimp
 """
 todo Refactor these functions, move to pyfimotopa/client.py
 """
+
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     global connected
@@ -16,14 +18,18 @@ def on_connect(client, userdata, flags, rc):
         connected = False
         print("MQTT client: Could not connect. Result code: " + str(rc))
 
+
 def on_disconnect(client, userdata, rc):
     global connected
 
     connected = False
     print("MQTT client: Disconnected... Result code: " + str(rc))
 
+
 def do_connect():
     client = mqtt.Client(client_id)
+    client.loop_start()
+
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
 
@@ -31,9 +37,14 @@ def do_connect():
     client.connect(server, port, 60)
     return client
 
+
 def serve(client, selected_devices):
     global connected
-    f = fimp.Client(client, selected_devices, token, ha_host, debug)
+    f = fimp.Client(
+        mqtt=client,
+        selected_devices=selected_devices,
+        debug=debug
+    )
 
     print("Sleeping forever...")
     while True:
@@ -42,22 +53,6 @@ def serve(client, selected_devices):
             break
 
         time.sleep(10)
-
-# def create_binary_sensor(client):
-#     """
-#     Test function for creating an binary sensor
-#     """
-#     t = "homeassistant/binary_sensor/garden/config"
-#     p = '{"name": "garden", "device_class": "motion", "state_topic": "homeassistant/binary_sensor/garden/state"}'
-#     client.publish(t, p)
-
-#     t = "homeassistant/binary_sensor/garden/state"
-#     p = "OFF"
-#     client.publish(t, p)
-
-#     time.sleep(3)
-#     p = "ON"
-#     client.publish(t, p)
 
 
 if __name__ == "__main__":
@@ -68,8 +63,6 @@ if __name__ == "__main__":
     username = os.environ.get('FIMPUSERNAME')
     password = os.environ.get('FIMPPASSWORD')
     port = int(os.environ.get('FIMPPORT'))
-    token = os.environ.get('HASSIO_TOKEN')
-    ha_host = os.environ.get('HA_HOST')
     client_id = os.environ.get('CLIENT_ID')
     debug = os.environ.get('DEBUG')
     selected_devices = os.environ.get('SELECTED_DEVICES')
@@ -89,8 +82,6 @@ if __name__ == "__main__":
     print('User: ', username)
     print('Port: ', port)
     print('Client id: ', client_id)
-    print('Home Assistant long-lived access token: ', token[:5] + "...")
-    print('Home Assistant host: ', ha_host)
     print('Debug : ', debug)
     print('Selected devices', selected_devices)
 
@@ -101,10 +92,8 @@ if __name__ == "__main__":
 
     elif len(sys.argv) > 1 and sys.argv[1] == "serve":
         client = do_connect()
-        client.loop_start()
 
         time.sleep(2)
 
         if connected:
             serve(client, selected_devices)
-            # serve(client, [7, 37])
